ba9db49f44210724a74ca38114680fe3f57d3b6f
Bogdoing <bogdoingg@gmail.com>
2023-02-04 13:18:53 +0300

    Added functions to activate windows

--- WindowsActivator/Program.cs
+++ WindowsActivator/Program.cs
@@ -30,13 +30,89 @@ namespace WindowsActivator
                     break;
                 default:
                     Console.WriteLine("Undefaint menu item");
+                    Console.ReadKey();
+                    break;
+            }                       
+            Console.WriteLine("End cmd.exe!");
+        }
+
+        private void setKey()
+        {
+            //"slmgr/ipk \"W269N-WFGWX-YVC9B-4J6C9-T83GX\"
+            Console.WriteLine(
+                "1 - Activator Win 10-11 Pro \r\n" +
+                "2 - systeminfo \r\n"
+            );
+
+            int menu_i = Convert.ToInt32(Console.ReadLine());
+
+            switch (menu_i)
+            {
+                case 1:
+                    Process.Start("cmd.exe", "/C " + "slmgr/ipk \"W269N-WFGWX-YVC9B-4J6C9-T83GX\"");
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Process.Start("cmd.exe", "/C " + "systeminfo");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Undefaint menu item");
+                    Console.ReadKey();
                     break;
             }
+        } 
+        private void setKMS()
+        {
+            //slmgr / skms kms.digiboy.ir
+            Console.WriteLine(
+                "1 - Activator Win 10-11 Pro \r\n" +
+                "2 - systeminfo \r\n"
+            );
 
-                       
-            Console.WriteLine("End cmd.exe!");
+            int menu_i = Convert.ToInt32(Console.ReadLine());
 
-            
+            switch (menu_i)
+            {
+                case 1:
+                    Process.Start("cmd.exe", "/C " + "slmgr / skms kms.digiboy.ir");
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Process.Start("cmd.exe", "/C " + "systeminfo");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Undefaint menu item");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+        private void setAuto()
+        {
+            //slmgr /ato
+            Console.WriteLine(
+                "1 - Activator Win 10-11 Pro \r\n" +
+                "2 - systeminfo \r\n"
+            );
+
+            int menu_i = Convert.ToInt32(Console.ReadLine());
+
+            switch (menu_i)
+            {
+                case 1:
+                    Process.Start("cmd.exe", "/C " + "slmgr /ato");
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Process.Start("cmd.exe", "/C " + "systeminfo");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Undefaint menu item");
+                    Console.ReadKey();
+                    break;
+            }
         }
     }
 }
