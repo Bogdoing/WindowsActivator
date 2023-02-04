c4f8f9af5946f1b7699c748a5d113a0006e6f242
Bogdoing <bogdoingg@gmail.com>
2023-02-04 12:30:35 +0300

    Добавьте файлы проекта.

---
+++ WindowsActivator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace WindowsActivator
+{
+    internal class Program
+    {
+
+        private string Activator_Win = "W269N-WFGWX-YVC9B-4J6C9-T83GX";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start cmd.exe!");
+
+            Process.Start("cmd.exe", "/C " + "systeminfo");
+
+            //Console.WriteLine("End cmd.exe!");
+
+            Console.ReadKey();
+        }
+    }
+}
