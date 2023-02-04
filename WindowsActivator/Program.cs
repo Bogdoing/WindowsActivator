2dfb1a8b7023224c0a575cfec58b4175ccb97637
Bogdoing <bogdoingg@gmail.com>
2023-02-04 12:54:32 +0300

    Added test systeminfo command

--- WindowsActivator/Program.cs
+++ WindowsActivator/Program.cs
@@ -4,18 +4,39 @@ namespace WindowsActivator
 {
     internal class Program
     {
-
-        private string Activator_Win = "W269N-WFGWX-YVC9B-4J6C9-T83GX";
+        //slmgr/ipk "W269N-WFGWX-YVC9B-4J6C9-T83GX"
+        //slmgr /skms kms.digiboy.ir
+        //slmgr /ato
+        private string Activator_Win_Key = "W269N-WFGWX-YVC9B-4J6C9-T83GX";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Start cmd.exe!");
 
-            Process.Start("cmd.exe", "/C " + "systeminfo");
+            Console.WriteLine(
+                "1 - Activator Win 10-11 Pro \r\n" +
+                "2 - systeminfo \r\n"
+            );
 
-            //Console.WriteLine("End cmd.exe!");
+            int menu_i = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadKey();
+            switch (menu_i)
+            {
+                case 1:
+                    break;
+                case 2:
+                    Process.Start("cmd.exe", "/C " + "systeminfo");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Undefaint menu item");
+                    break;
+            }
+
+                       
+            Console.WriteLine("End cmd.exe!");
+
+            
         }
     }
 }
